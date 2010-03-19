95cfde54e66bbfb8598d4207bad52cb4e8d1373c
ivan.ivanenko <ivan.ivanenko@bdd3a5e6-2252-11df-8867-651e7203ed55>
2010-03-19 22:40:45 +0000

    demo setupe

--- src/setup.py
+++ src/setup.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+import glob
+import os
 
 setup(name='FoobNix',
       version='1.0',
@@ -8,5 +10,39 @@ setup(name='FoobNix',
       author='Ivan Ivanenko',
       author_email='ivan.ivanenko@gmail.com',
       url='http://code.google.com/p/foobnix/',
-      packages=[],
+      package_data={'foobnix': ['glade/*.glade']},
+
+      packages=[".",
+                "foobnix", 
+                "foobnix.application",
+                "foobnix.directory",
+                "foobnix.glade",
+                "foobnix.model",                
+                "foobnix.online",
+                "foobnix.player",
+                "foobnix.playlist",
+                "foobnix.preferences",
+                "foobnix.radio",
+                "foobnix.tryicon",
+                "foobnix.util",
+                "foobnix.window",
+                ],  
+        scripts = ['foobnix.py'],
+        py_modules=[".",
+                "foobnix", 
+                "foobnix.application",
+                "foobnix.directory",
+                "foobnix.glade",
+                "foobnix.model",                
+                "foobnix.online",
+                "foobnix.player",
+                "foobnix.playlist",
+                "foobnix.preferences",
+                "foobnix.radio",
+                "foobnix.tryicon",
+                "foobnix.util",
+                "foobnix.window",
+                ]    
      )
+
+
