dbf9911e5d428e4512ce77bb5d4d300cb15ef9da
ivan.ivanenko <ivan.ivanenko@bdd3a5e6-2252-11df-8867-651e7203ed55>
2010-06-01 19:26:12 +0000

    fix google search

---
+++ src/foobnix/online/google_search.py
@@ -0,0 +1,38 @@
+'''
+Created on 01.06.2010
+
+@author: ivan
+'''
+from foobnix.online.google.search import GoogleSearch, SearchError
+import time
+def googleHelp(query):
+    print "Not Found, wait for results from google ..."
+    results = []
+    ask = query.encode('utf-8')
+    print ask
+
+    gs = GoogleSearch(ask,True, True)
+    print gs
+    gs.results_per_page = 10
+    results = gs.get_results()
+    print results
+    for res in results:
+        result = res.title.encode('utf8')
+        time.sleep(0.05)
+        results.append(str(result))
+    
+#print googleHelp("madoonna 1")
+def search():
+    print "Begin"
+    gs = GoogleSearch("quick and dirty")
+    gs.results_per_page = 5
+    results = gs.get_results()
+    print results
+    for res in results:
+        print res.title.encode("utf8")
+        print res.desc.encode("utf8")
+        print res.url.encode("utf8")
+
+search()
+        
+        
