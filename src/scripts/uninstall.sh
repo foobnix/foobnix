42fa0145decbe72a7b2ef3676d629b57a2f889ad
ivan.ivanenko <ivan.ivanenko@bdd3a5e6-2252-11df-8867-651e7203ed55>
2010-07-16 21:15:00 +0000

    uninstall

--- src/scripts/uninstall.sh
+++ src/scripts/uninstall.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-cd .. / 
+cd ../
 
-python setup.py install - -record files.txt
-cat files.txt | sudo xargs rm - rf
+python setup.py install --record files.txt
+cat files.txt | sudo xargs rm -rf
+rm files.txt
