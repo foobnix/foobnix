38b5b09eb15102ff4764ea379f12610174792f58
ivan.ivanenko <ivan.ivanenko@bdd3a5e6-2252-11df-8867-651e7203ed55>
2010-08-25 20:20:54 +0000

    fix script

--- src/scripts/to-upload-current.sh
+++ src/scripts/to-upload-current.sh
@@ -7,15 +7,22 @@ LANG=en
 REV='today'
 echo "REVISION" 
 echo $REV
-$(svn info > svn_info.txt)
+svn info > svn_info.txt
+date >> svn_info.txt
 
+
+rm -rf ../deb/*.*
 
 cp -r . ../foobnix_$REV
 tar cvzf ../deb/foobnix_$REV.tar.gz ../foobnix_$REV --exclude=.svn --exclude=*.pyc 
 rm -rf ../foobnix_$REV
 
 
-python scripts/upload.py --summary=foobnix_$REV.tar.gz --project=foobnix --user=ivan.ivanenko@gmail.com --password=$PASSWORD ../deb/foobnix_$REV.tar.gz
-rm -rf ../deb/foobnix_$REV.tar.gz
+#python scripts/upload.py --summary=foobnix_$REV.tar.gz --project=foobnix --user=ivan.ivanenko@gmail.com --password=$PASSWORD ../deb/foobnix_$REV.tar.gz
+#rm -rf ../deb/foobnix_$REV.tar.gz
 
 rm  svn_info.txt
+
+cd ../deb
+svn commit (c) ../deb/foobnix_$REV.tar.gz 
+
