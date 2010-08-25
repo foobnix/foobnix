82962331e5d3e69b08cc7e78062577e16187fa99
ivan.ivanenko <ivan.ivanenko@bdd3a5e6-2252-11df-8867-651e7203ed55>
2010-08-25 17:35:51 +0000

    fix path

--- src/foobnix/application/app_view.py
+++ src/foobnix/application/app_view.py
@@ -4,6 +4,7 @@ Created on Mar 14, 2010
 @author: ivan
 '''
 import gtk.glade
+import sys
 class AppView():
 
     gladeMain = "foobnix/glade/foobnix.glade"
@@ -32,9 +33,9 @@ class AppView():
             return gtk.glade.XML(main, widget, domain)
         except:
             try:
-                return gtk.glade.XML("/usr/local/lib/python2.6/dist-packages/" + main, widget, domain)
+                return gtk.glade.XML(sys.exec_prefix + '/lib/python' + sys.version[:3] + '/dist-packages/' + main, widget, domain)
             except:
-                return gtk.glade.XML("/usr/lib/python2.5/site-packages/" + main, widget, domain)
+                return gtk.glade.XML(sys.exec_prefix + '/lib/python' + sys.version[:3] + '/site-packages/' + main, widget, domain)
             
         
   
