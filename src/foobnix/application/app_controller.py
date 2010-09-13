5aa6511521f8f17c4ce7b9f873c9090f8e23edea
anton.komolov <anton.komolov@bdd3a5e6-2252-11df-8867-651e7203ed55>
2010-09-13 21:35:06 +0000

    Add save / restore settings for hpaned/vpaned

--- src/foobnix/application/app_controller.py
+++ src/foobnix/application/app_controller.py
@@ -31,43 +31,43 @@ class AppController(BaseController):
 
     def __init__(self, v):
         BaseController.__init__(self)
-        
+
         last_fm_connector = LastFmConnector()
-                
+
         self.player_controller = PlayerController(last_fm_connector)
-        
+
         #self.playlistCntr = PlaylistCntr(v.playlist, self.player_controller)
         self.onlineCntr = OnlineListCntr(v.gxMain, self.player_controller, last_fm_connector)
-        
-        self.playlistCntr = self.onlineCntr 
-        
+
+        self.playlistCntr = self.onlineCntr
+
         self.virtualListCntr = VirturalLIstCntr()
-        
+
         #self.radioListCntr = RadioListCntr(v.gxMain, self.player_controller)
-        
+
         self.playerWidgets = PlayerWidgetsCntl(v.gxMain, self.player_controller)
         self.player_controller.registerWidgets(self.playerWidgets)
         self.player_controller.registerPlaylistCntr(self.playlistCntr)
-        
-        
+
+
         self.directoryCntr = DirectoryCntr(v.gxMain, self.playlistCntr, self.virtualListCntr)
         #self.playlistCntr.registerDirectoryCntr(self.directoryCntr)
         self.appConfCntr = AppConfigurationCntrl(v.gxMain, self.directoryCntr)
         self.onlineCntr.register_directory_cntr(self.directoryCntr)
-        
+
         self.player_controller.registerOnlineCntr(self.onlineCntr)
-        
-        
-        
+
+
+
         self.main_window_controller = WindowController(v.gxMain, v.gxAbout)
-        
-        
+
+
         """show pref window"""
-        
-        
+
+
         menu_preferences = v.gxMain.get_widget("menu_preferences")
         menu_preferences.connect("activate", lambda * a:self.pref.show())
-        
+
         self.tray_icon = TrayIcon(v.gxTrayIcon)
         self.main_window_controller.tray_icon = self.tray_icon
         self.tray_icon.connect('toggle_window_visibility', self.main_window_controller.toggle_visibility)
@@ -78,49 +78,49 @@ class AppController(BaseController):
         self.tray_icon.connect('next', self.player_controller.next)
         self.tray_icon.connect('volume_up', self.player_controller.volume_up)
         self.tray_icon.connect('volume_down', self.player_controller.volume_down)
-        
+
         self.player_controller.connect('song_playback_started', self.tray_icon.on_song_started)
         self.player_controller.connect('song_playback_started', self.main_window_controller.on_song_started)
 
         self.main_window_controller.connect('exit', self.exit)
-        
+
         self.search_panel = SearchPanel(v.gxMain, last_fm_connector)
         self.search_panel.connect('show_search_results', self.onlineCntr.show_results)
         self.search_panel.connect('show_searching_line', self.onlineCntr.show_searching)
-        
+
         self.pref = PreferencesWindow(self.directoryCntr, self.onlineCntr, self.tray_icon)
         last_fm_connector.preferences_window = self.pref
-        
+
         self.restore_state()
-        
+
         """paly music via arguments"""
         self.play_arguments(sys.argv)
         self.main_window_controller.show()
-        """enable proxy"""        
-      
+        """enable proxy"""
+
         self.check_version()
-        
-        
-    def check_version(self):        
+
+
+    def check_version(self):
         uuid = FConfiguration().uuid
         current_version = VERSION
         try:
             f = urllib2.urlopen("http://www.foobnix.com/version?uuid=" + uuid + "&host=" + gethostname())
-        except:            
+        except:
             return None
-        
+
         new_version = f.read()
         LOG.info("versions", current_version , "|", new_version, "|")
         f.close()
         if FConfiguration().check_new_version and current_version < new_version:
             info_dialog_with_link(_("New version is available"), "foobnix " + new_version, "http://www.foobnix.com/?page=download")
             #self.setStatusText(_("New version ")+new_version+_(" avaliable at www.foobnix.com"));
-             
-    
+
+
     def action_command(self, args):
         if args:
             if len(args) == 1:
-                command = args[0]                
+                command = args[0]
             elif len(args) == 2:
                 command = args[1]
             else:
@@ -130,13 +130,13 @@ class AppController(BaseController):
             elif "--prev" == command:
                 self.player_controller.prev()
             elif "--stop" == command:
-                self.player_controller.stopState()    
+                self.player_controller.stopState()
             elif "--pause" == command:
                 self.player_controller.pauseState()
             elif "--play" == command:
                 self.player_controller.playState()
             elif "--volume-up" == command:
-                self.player_controller.volume_up()             
+                self.player_controller.volume_up()
             elif "--volume-down" == command:
                 self.player_controller.volume_down()
             elif "--show-hide" == command:
@@ -148,63 +148,67 @@ class AppController(BaseController):
                     self.player_controller.playState()
             return True
         return False
-    
+
     def play_arguments(self, args):
-        #gtk.gdk.threads_leave()     
+        #gtk.gdk.threads_leave()
         gtk.gdk.threads_enter() #@UndefinedVariable
         if not self.action_command(args):
             self.main_window_controller.show()
         self.onlineCntr.on_play_argumens(args)
-        gtk.gdk.threads_leave() 
-    
+        gtk.gdk.threads_leave()
+
     def exit(self, *a):
         self.main_window_controller.hide()
-        self.save_state()        
+        self.save_state()
         gtk.main_quit()
-    
+
     def restore_state(self):
         if FConfiguration().playlistState:
             self.playlistCntr.setState(FConfiguration().playlistState)
-        
+
         if FConfiguration().virtualListState:
             self.directoryCntr.setState(FConfiguration().virtualListState)
-        
+
         if FConfiguration().volumeValue:
             self.playerWidgets.volume.set_value(FConfiguration().volumeValue)
             self.player_controller.setVolume(FConfiguration().volumeValue / 100)
-        
+
         if FConfiguration().radiolistState:
             self.radioListCntr.setState(FConfiguration().radiolistState)
-        
+
         if FConfiguration().save_tabs:
             self.onlineCntr.append_notebook_page(FConfiguration().last_notebook_page)
-            
+
             if FConfiguration().play_on_start:
                 self.onlineCntr.append_and_play(FConfiguration().last_notebook_beans, FConfiguration().last_play_bean)
             else:
                 self.onlineCntr.append(FConfiguration().last_notebook_beans)
-                
-        
+
+
         self.main_window_controller.on_load()
-        
-                    
-        
-    
+
+
+
+
     def save_state(self):
         FConfiguration().last_notebook_page = self.onlineCntr.last_notebook_page
         FConfiguration().last_notebook_beans = self.onlineCntr.last_notebook_beans
         FConfiguration().last_play_bean = self.onlineCntr.index
-        
+
         FConfiguration().playlistState = self.playlistCntr.getState()
         FConfiguration().virtualListState = self.directoryCntr.getState()
-        
+
         #FConfiguration().radiolistState = self.radioListCntr.getState()
-        
+
         FConfiguration().volumeValue = self.playerWidgets.volume.get_value()
-        
+
+        if self.playerWidgets.vpanel.get_position() > 0:
+            FConfiguration().vpanelPostition = self.playerWidgets.vpanel.get_position()
         if self.playerWidgets.hpanel.get_position() > 0:
-            FConfiguration().hpanelPostition = self.playerWidgets.hpanel.get_position()        
-        
+            FConfiguration().hpanelPostition = self.playerWidgets.hpanel.get_position()
+        if self.playerWidgets.hpanel.get_position() > 0:
+            FConfiguration().hpanel2Postition = self.playerWidgets.hpanel2.get_position()
+
         self.main_window_controller.on_save()
         FConfiguration().save()
 
--- src/foobnix/util/configuration.py
+++ src/foobnix/util/configuration.py
@@ -29,47 +29,47 @@ def get_version():
         version_file = os.path.join(FOOBNIX_TMP, FOOBNIX_VERSION_FILE_NAME)
     elif os.path.exists(FOOBNIX_VERSION_FILE_NAME):
         version_file = os.path.join(FOOBNIX_VERSION_FILE_NAME)
-     
+
     with file(version_file, 'r') as v_file:
-        
-        for line in v_file:      
-            line = str(line).strip()     
-            if line.find("VERSION=") >= 0:             
+
+        for line in v_file:
+            line = str(line).strip()
+            if line.find("VERSION=") >= 0:
                 result = line[len("VERSION="):]
-            elif line.find("RELEASE=") >= 0:  
-                result += "-" + line[len("RELEASE="):]  
+            elif line.find("RELEASE=") >= 0:
+                result += "-" + line[len("RELEASE="):]
     return result
 
-VERSION = get_version()        
+VERSION = get_version()
 
 def check_create_cfg_dir():
     if not os.path.exists(CFG_FILE_DIR):
         os.makedirs(CFG_FILE_DIR)
 
 class FConfiguration:
-    
-    
-    
+
+
+
     FOOBNIX = "foobnix"
     SUPPORTED_AUDIO_FORMATS = 'supported_audio_formats'
-    
+
     __metaclass__ = Singleton
-    
-    
-    #config = ConfigParser.RawConfigParser()    
+
+
+    #config = ConfigParser.RawConfigParser()
     #config.read(CFG_FILE)
-    
-    
+
+
     #def get(self, type):
     #    return self.config.get(self.FOOBNIX, self.SUPPORTED_AUDIO_FORMATS)
     check_create_cfg_dir()
     def __init__(self, is_load_file=True):
-        
+
         self.media_library_path = [tempfile.gettempdir()]
-        
+
         self.onlineMusicPath = tempfile.gettempdir()
         self.supportTypes = [".mp3", ".ogg", ".ape", ".flac", ".wma", ".cue", ".mpc", ".aiff", ".raw", ".au", ".aac", ".mp4", ".ra", ".m4p", ".3gp" ]
-        
+
         self.isRandom = False
         self.isRepeat = True
         self.isPlayOnStart = False
@@ -80,90 +80,90 @@ class FConfiguration:
         self.vpanelPostition = 234
         self.hpanelPostition = 370
         self.hpanel2Postition = 521
-        
+
         ### view panels ###
         self.view_tree_panel = True
         self.view_search_panel = True
         self.view_info_panel = False
         self.view_lyric_panel = False
-        
+
         self.playlistState = None
         self.radiolistState = None
         self.virtualListState = {"Default list" : []}
-        
-        
+
+
         self.is_save_online = False
         self.song_source_relevance_algorithm = 0
         self.online_tab_show_by = 0
-        
+
         self.vk_login = "qax@bigmir.net"
         self.vk_password = "foobnix"
-        
-        self.lfm_user_default = "l_user_" 
+
+        self.lfm_user_default = "l_user_"
         self.lfm_login = self.lfm_user_default
         self.lfm_password = "l_pass_"
-        
+
         self.API_KEY = "bca6866edc9bdcec8d5e8c32f709bea1"
         self.API_SECRET = "800adaf46e237805a4ec2a81404b3ff2"
-    
-        self.cookie = None 
-        
+
+        self.cookie = None
+
         self.count_of_tabs = 3
         self.len_of_tab = 30
-        
+
         self.cache_music_beans = []
-        
+
         self.tab_position = "top"
-        
+
         self.last_dir = None
-        
+
         self.on_close_window = const.ON_CLOSE_HIDE;
         self.show_tray_icon = True
-        
+
         self.proxy_enable = False
         self.proxy_url = None
         self.proxy_user = None
         self.proxy_password = None
-        
+
         """info panel"""
-        
+
         if gtk.gdk.screen_height() < 800:
             self.info_panel_image_size = 150
         else:
-            self.info_panel_image_size = 200    
-                
-        
+            self.info_panel_image_size = 200
+
+
         self.tab_close_element = "label"
-        self.play_ordering = const.ORDER_LINEAR 
+        self.play_ordering = const.ORDER_LINEAR
         self.play_looping = const.LOPPING_LOOP_ALL
-        
+
         """random uuis of player"""
         self.uuid = uuid.uuid4().hex
-        
+
         self.check_new_version = True
-        
+
         self.add_child_folders = True
         self.lyric_panel_image_size = 250
-        
+
         self.tray_icon_auto_hide = True
-        
+
         self.action_hotkey = {'foobnix --volume-up': '<SUPER>Up', 'foobnix --volume-down': '<SUPER>Down', 'foobnix --show-hide': '<SUPER>a', 'foobnix --prev': '<SUPER>Left', 'foobnix --play': '<SUPER>x', 'foobnix --pause': '<SUPER>z', 'foobnix --next': '<SUPER>Right'}
-        
-        
+
+
         self.last_notebook_page = "Foobnix"
         self.last_notebook_beans = []
         self.last_play_bean = 0
         self.save_tabs = True
         self.play_on_start = True
         self.configure_state = None
-   
+
         instance = self._loadCfgFromFile(is_load_file)
         if instance:
             try:
                 self.supportTypes = instance.supportTypes
                 self.virtualListState = instance.virtualListState
                 self.playlistState = instance.playlistState
-                self.radiolistState = instance.radiolistState 
+                self.radiolistState = instance.radiolistState
                 self.media_library_path = instance.media_library_path
                 self.isRandom = instance.isRandom
                 self.isRepeat = instance.isRepeat
@@ -174,67 +174,67 @@ class FConfiguration:
                 self.vpanelPostition = instance.vpanelPostition
                 self.hpanelPostition = instance.hpanelPostition
                 self.hpanel2Postition = instance.hpanel2Postition
-                
+
                 self.savedRadioList = instance.savedRadioList
-                
+
                 self.is_save_online = instance.is_save_online
                 self.onlineMusicPath = instance.onlineMusicPath
-                
+
                 self.vk_login = instance.vk_login
                 self.vk_password = instance.vk_password
-                
+
                 self.lfm_login = instance.lfm_login
                 self.lfm_password = instance.lfm_password
-                
+
                 self.count_of_tabs = instance.count_of_tabs
                 self.len_of_tab = instance.len_of_tab
-                
+
                 self.cookie = instance.cookie
-                
+
                 self.view_tree_panel = instance.view_tree_panel
                 self.view_search_panel = instance.view_search_panel
                 self.view_info_panel = instance.view_info_panel
                 self.view_lyric_panel = instance.view_lyric_panel
-                
+
                 self.cache_music_beans = instance.cache_music_beans
                 self.tab_position = instance.tab_position
-                
+
                 self.last_dir = instance.last_dir
                 self.info_panel_image_size = instance.info_panel_image_size
                 self.tab_close_element = instance.tab_close_element
-                
-                self.play_ordering = instance.play_ordering 
+
+                self.play_ordering = instance.play_ordering
                 self.play_looping = instance.play_looping
-                
+
                 self.on_close_window = instance.on_close_window;
                 self.show_tray_icon = instance.show_tray_icon
-                
+
                 "proxy"
                 self.proxy_enable = instance.proxy_enable
                 self.proxy_url = instance.proxy_url
                 self.proxy_user = instance.proxy_user
                 self.proxy_password = instance.proxy_password
-                
+
                 self.uuid = instance.uuid
-                
-                self.check_new_version = instance.check_new_version 
+
+                self.check_new_version = instance.check_new_version
                 self.add_child_folders = instance.add_child_folders
                 self.lyric_panel_image_size = instance.lyric_panel_image_size
-                
+
                 self.action_hotkey = instance.action_hotkey
                 self.tray_icon_auto_hide = instance.tray_icon_auto_hide
-                
+
                 self.last_notebook_page = instance.last_notebook_page
                 self.last_notebook_beans = instance.last_notebook_beans
                 self.play_on_start = instance.play_on_start
                 self.save_tabs = instance.save_tabs
                 self.last_play_bean = instance.last_play_bean
                 self.configure_state = instance.configure_state
-                
+
             except AttributeError:
                 LOG.debug("Configuraton attributes are changed")
                 os.remove(CFG_FILE)
- 
+
         LOG.info("LOAD CONFIGS")
         self.printArttibutes()
 
@@ -242,16 +242,16 @@ class FConfiguration:
         LOG.info("SAVE CONFIGS")
         self.printArttibutes()
         FConfiguration()._saveCfgToFile()
-        
+
     def printArttibutes(self):
         for i in dir(self):
             if not i.startswith("__"):
                 value = str(getattr(self, i))[:300]
                 LOG.info(i, value)
-        
+
     def _saveCfgToFile(self):
         #conf = FConfiguration()
-        
+
         save_file = file(CFG_FILE, 'w')
         try:
             pickle.dump(self, save_file)
@@ -259,23 +259,23 @@ class FConfiguration:
             LOG.error("Erorr dumping pickle conf")
         save_file.close()
         LOG.debug("Save configuration")
-            
+
     def _loadCfgFromFile(self, is_load_file):
         if not is_load_file:
             return
 
         try:
             with file(CFG_FILE, 'r') as load_file:
-                load_file = file(CFG_FILE, 'r') 
+                load_file = file(CFG_FILE, 'r')
                 pickled = load_file.read()
                 # fixing mistyped 'configuration' package name
                 if 'confguration' in pickled:
                     pickled = pickled.replace('confguration', 'configuration')
                 return pickle.loads(pickled)
-        
+
         except IOError:
             LOG.debug('Configuration file does not exist.')
-        except ImportError, ex:            
+        except ImportError, ex:
             LOG.error('Configuration file is corrupted. Removing it...')
             os.remove(CFG_FILE)
         except BaseException, ex:
