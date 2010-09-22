c2002b3fa4428904f3190e37e6d66bde0c8214c2
ivan.ivanenko@gmail.com <ivan.ivanenko@gmail.com@bdd3a5e6-2252-11df-8867-651e7203ed55>
2010-09-22 15:35:19 +0000

    create nice menu

--- src/foobnix/regui/menu.py
+++ src/foobnix/regui/menu.py
@@ -6,8 +6,10 @@ Created on Sep 22, 2010
 import gtk
 class MenuWidget():
     def __init__(self):
+        """TOP menu constructor"""
+        
         top = TopMenu()
-        """File menu"""
+        """File"""
         file = top.append("File")
         file.add_image_item("Add File(s)", gtk.STOCK_OPEN)
         file.add_image_item("Add Folder(s)", gtk.STOCK_OPEN)     
@@ -15,7 +17,7 @@ class MenuWidget():
         file.add_image_item("Quit", gtk.STOCK_QUIT)
         
         
-        """View menu"""
+        """View"""
         view = top.append("View")
         view.add_ckeck_item("Music Tree",True)
         view.add_ckeck_item("Search Panel",True)
@@ -25,28 +27,32 @@ class MenuWidget():
         view.separator()
         view.add_image_item("Preferences",gtk.STOCK_PREFERENCES)        
         
-        """Playbacka menu"""
+        """Playback"""
         playback = top.append("Playback")      
+        
+        """Playback - Order"""
         order = playback.add_text_item("Order")
         linear = order.add_radio_item("Linear",None, True)
         order.add_radio_item("Random",linear, True)
         order.separator()
         order.add_image_item("Shuffle",gtk.STOCK_UNDELETE)
         
-              
+        """Playback - Repeat"""      
         repeat = playback.add_text_item("Repeat")
         all = repeat.add_radio_item("All",None, True) 
         repeat.add_radio_item("Single",all, True)
         repeat.add_radio_item("Disable",all, True)
         
-        """Help menu"""
+        """Help"""
         help = top.append("Help")
         help.add_image_item("About",gtk.STOCK_ABOUT)
         help.add_image_item("Help",gtk.STOCK_HELP)
-    
+        
+        top.decorate()        
         self.widget = top.widget
         
 
+"""My custom menu class for helping buildings"""
 class MyMenu(gtk.Menu):
     def __init__(self):
         gtk.Menu.__init__(self)
@@ -88,12 +94,36 @@ class MyMenu(gtk.Menu):
         
         return menu
 
+"""My top menu bar helper"""
 class TopMenu():
     def __init__(self):
+        rc_st = '''
+            style "menubar-style" { 
+                GtkMenuBar::shadow_type = none
+                GtkMenuBar::internal-padding = 0
+                }
+            class "GtkMenuBar" style "menubar-style"
+        '''
+        gtk.rc_parse_string(rc_st)
+        
+        
         self.menu_bar = gtk.MenuBar()
         self.menu_bar.show()
        
         self.widget = self.menu_bar
+    
+    def decorate(self):
+        label = gtk.Label("")
+        style = label.get_style()
+        background_color = style.bg[gtk.STATE_NORMAL]
+        text_color = style.fg[gtk.STATE_NORMAL]
+       
+        self.menu_bar.modify_bg(gtk.STATE_NORMAL, background_color)
+
+        # making main menu look a bit better
+        for item in self.menu_bar.get_children():
+            current = item.get_children()[0]
+            current.modify_fg(gtk.STATE_NORMAL, text_color)
         
     def append(self, title):
         menu = MyMenu()
