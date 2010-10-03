5d4b8d5398887ee2cb10b93a3d33af56b13a967f
anton.komolov <anton.komolov@bdd3a5e6-2252-11df-8867-651e7203ed55>
2010-10-03 00:28:50 +0000

    fix

--- src/foobnix.py
+++ src/foobnix.py
@@ -4,13 +4,22 @@ Created on Sep 30, 2010
 @author: ivan
 '''
 import time
-import gobject
-from foobnix.regui.foobnix_core import FoobnixCore
-import gtk
+from foobnix.regui.controls.dbus_manager import foobnixDBusInterface
 
 init_time = time.time()
-gobject.threads_init() #@UndefinedVariable
-#gtk.gdk.threads_enter()
-eq = FoobnixCore()
-print "******Foobnix run in", time.time() - init_time, " seconds******"
-gtk.main()
+
+iface = foobnixDBusInterface()
+
+if not iface:
+    print "start server"
+    import gobject
+    from foobnix.regui.foobnix_core import FoobnixCore
+    import gtk
+    gobject.threads_init() #@UndefinedVariable
+    #gtk.gdk.threads_enter()
+    eq = FoobnixCore()
+    print "******Foobnix run in", time.time() - init_time, " seconds******"
+    gtk.main()
+else:
+    print "start client"
+    
