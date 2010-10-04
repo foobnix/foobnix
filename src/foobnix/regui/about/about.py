7a9df2012fad34eaf289f6ece215426f5a768789
zavlab1 <zavlab1@bdd3a5e6-2252-11df-8867-651e7203ed55>
2010-10-04 16:15:34 +0000

    Add Changelog-window, refactoring about.py

--- src/foobnix/regui/about/about.py
+++ src/foobnix/regui/about/about.py
@@ -6,12 +6,22 @@ Created on Oct 2, 2010
 import gtk
 from foobnix.regui.service.image_service import get_foobnix_pixmap_path_by_name
 
+def CreateButton_with_label_and_icon(image,label): 
+    box = gtk.HBox(False, 0)
+    box.set_border_width (2)
+    box.pack_end (label, True, False, 0)
+    box.pack_end (image, True, False, 0)
+    
+    button = gtk.Button()
+    button.add(box)
+    return button
+    
 def about():
     window = gtk.Window(gtk.WINDOW_TOPLEVEL)
     window.set_title ("About Foobnix")
     window.set_position(gtk.WIN_POS_CENTER)
-    window.set_border_width(10)
-    window.set_geometry_hints(window, min_width=270, min_height=300)
+    window.set_border_width(5)
+    window.set_geometry_hints(window, min_width=270, min_height=270)
     gtk.window_set_default_icon_from_file (get_foobnix_pixmap_path_by_name("foobnix.png"))
     window.set_resizable(False)
     
@@ -20,58 +30,52 @@ def about():
     
     window.set_icon_from_file (foobnix_image_path)
     
-    table = gtk.Table(2, 3, False)
-    table.set_col_spacing(0, 50)
+    table = gtk.Table(3, 3, False)
+    #table.set_border_width(0)
+    #table.set_col_spacing(0, 50)
     
     image = gtk.image_new_from_file(foobnix_image_path);
-    table.attach(image, 0, 2, 0, 1)
+    table.attach(image, 0, 3, 0, 1)
     
     label = gtk.Label("Foobnix")
     
     label.set_markup ("""
-    <big><big><b><b>Foobnix</b></b></big></big>
-    Playing all imaginations\n
-    <small>Developed by Ivan Ivanenko</small>
-    <small>ivan.ivanenko@gmail.com</small>\n   
-    <a href="http://www.foobnix.com">www.foobnix.com</a>
-    """);
+<big><big><b><b>Foobnix</b></b></big></big>
+Playing all imaginations\n
+<small>Developed by Ivan Ivanenko</small>
+<small>ivan.ivanenko@gmail.com</small>\n   
+<a href="http://www.foobnix.com">www.foobnix.com</a>\n""");
     
     label.set_justify(gtk.JUSTIFY_CENTER)
-    table.attach(label, 0, 2, 1, 2)
+    table.attach(label, 0, 3, 1, 2)
     
     label = gtk.Label("Credits")
     image = gtk.image_new_from_stock(gtk.STOCK_INFO, gtk.ICON_SIZE_MENU)
     
-    box = gtk.HBox(False, 0)
-    box.set_border_width (2)
-    box.pack_end (label, True, False, 0)
-    box.pack_end (image, True, False, 0)
-    
-    button = gtk.Button()
-    button.set_border_width (5)
-    button.add(box)
-    table.attach(button, 0, 1, 2, 3)
+    button_credits = CreateButton_with_label_and_icon(image,label)
+    button_credits.set_border_width (9)
+    table.attach(button_credits, 0, 1, 2, 3)
     
     label = gtk.Label("Close")
     image = gtk.image_new_from_stock(gtk.STOCK_STOP, gtk.ICON_SIZE_MENU)
     
-    box = gtk.HBox(False, 0)
-    box.set_border_width (2)
-    box.pack_end (label, True, False, 0)
-    box.pack_end (image, True, False, 0)
+    button_close = CreateButton_with_label_and_icon(image,label)
+    button_close.set_border_width (9)
+    table.attach(button_close, 2, 3, 2, 3)
     
-    button = gtk.Button()
-    button.set_border_width (5)
-    button.add(box)
-    table.attach(button, 1, 2, 2, 3)
+    label = gtk.Label("Changelog")
+    image = gtk.image_new_from_stock(gtk.STOCK_DND, gtk.ICON_SIZE_MENU)
+    
+    button_changelog = CreateButton_with_label_and_icon(image,label)
+    button_changelog.set_border_width (9)
+    table.attach(button_changelog, 1, 2, 2, 3)
     
     window.connect("destroy", lambda * x:window.hide())
-    button.connect("clicked", lambda * x:window.hide())
-    button.grab_focus ()
+    button_close.connect("clicked", lambda * x:window.hide())
+    button_close.grab_focus ()
     window.add(table)
     window.show_all()
     
-
 
 
 
--- src/foobnix/regui/about/changelog.py
+++ src/foobnix/regui/about/changelog.py
@@ -1,9 +1,10 @@
 import gtk
+import pango
 from foobnix.regui.service.image_service import get_foobnix_pixmap_path_by_name
 
-def credits():
+def changelog():
     window = gtk.Window(gtk.WINDOW_TOPLEVEL)
-    window.set_title ("Credits")
+    window.set_title ("Changelog")
     window.set_position(gtk.WIN_POS_CENTER)
     window.set_border_width(10)
     window.set_default_size(500,200)
@@ -13,14 +14,17 @@ def credits():
     
     window.connect("destroy", lambda * x:window.hide())
     
-    buffer=gtk.TextBuffer()
-    buffer_content=""
-    buffer_content.set_makeup("""\t\t\tDevelopers:
-Ivan Ivanenko <ivan.ivanenko@gmail.com>
-Anton.Komolov <anton.komolov@gmail.com>
-Dmitry Kozhura <Dmitry-Kogura@yandex.ru>""")
+    bold_tag=gtk.TextTag("bold")
+    bold_tag.weight=pango.WEIGHT_BOLD
+    texttagtable = gtk.TextTagTable()
+    texttagtable.add (bold_tag)
+    buffer=gtk.TextBuffer(texttagtable)
+    buffer_content="""\t\t\tChangelog of Foobnix (since 2.0.9 version)"""
     buffer.set_text(buffer_content)
+    buffer.apply_tag(bold_tag, buffer.get_start_iter(), buffer.get_end_iter())
+    
     text=gtk.TextView(buffer)
+    text.set_editable(False)
     scrolled_window.add(text)
     button=gtk.Button("Close",gtk.STOCK_CLOSE)
     
@@ -37,6 +41,5 @@ Dmitry Kozhura <Dmitry-Kogura@yandex.ru>""")
     window.show_all()
     
     
-credits()
+changelog()
 gtk.main()
-    
