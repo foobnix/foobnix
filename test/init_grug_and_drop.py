5dedef28aadf714264db5bbae7f2d25cfe9c1df6
ivan.ivanenko <ivan.ivanenko@bdd3a5e6-2252-11df-8867-651e7203ed55>
2010-10-05 20:02:08 +0000

    create drug and drop

--- test/init_grug_and_drop.py
+++ test/init_grug_and_drop.py
@@ -1,4 +1,5 @@
 import gtk
+import gobject
 
 class BaseTree(gtk.TreeView):
     def __init__(self):
@@ -9,21 +10,27 @@ class BaseTree(gtk.TreeView):
         column = gtk.TreeViewColumn("value", gtk.CellRendererText(), text=1)
         self.append_column(column)
         
-        model = gtk.TreeStore(int, str)
+        model = gtk.TreeStore(int, str, gobject.TYPE_BOOLEAN)
 
-        for i, item in enumerate(["zero", "one", "two", "three", "four", "five", "six"]):
-            model.append(None, [i, item])
+        filter_model = model.filter_new()        
+        filter_model.set_visible_column(2)
+        self.set_model(filter_model)
+
+        for i, item in enumerate(["zero", "one", "two", "three", "four", "five", "six"]):            
+            model.append(None, [i, item, i % 2])
         
-        self.set_model(model)
         
         self.enable_model_drag_source(gtk.gdk.BUTTON1_MASK, [("example", 0, 0)], gtk.gdk.ACTION_COPY)
         self.enable_model_drag_dest([("example", 0, 0)], gtk.gdk.ACTION_COPY)
         
         self.connect("drag_drop", self.on_drag_drop)
         
+        self.get_selection().set_mode(gtk.SELECTION_MULTIPLE)
+        
+        
     def iter_copy(self, from_model, from_iter, to_model, to_iter, pos):   
 
-        row = [from_model.get_value(from_iter, 0), from_model.get_value(from_iter, 1)]
+        row = [from_model.get_value(from_iter, 0), from_model.get_value(from_iter, 1), True]
         
         if (pos == gtk.TREE_VIEW_DROP_INTO_OR_BEFORE) or (pos == gtk.TREE_VIEW_DROP_INTO_OR_AFTER):
             new_iter = to_model.prepend(to_iter, row)
@@ -40,9 +47,11 @@ class BaseTree(gtk.TreeView):
                 self.iter_copy(from_model, next_iter_to_copy, to_model, new_iter, gtk.TREE_VIEW_DROP_INTO_OR_BEFORE)
     
     def on_drag_drop(self, to_tree, drag_context, x, y, selection):
-        to_model = to_tree.get_model()
+        to_filter_model = to_tree.get_model()
+        to_model = to_filter_model.get_model()
         if to_tree.get_dest_row_at_pos(x, y):
-            to_path, to_pos = to_tree.get_dest_row_at_pos(x, y)      
+            to_path, to_pos = to_tree.get_dest_row_at_pos(x, y)
+            to_path = to_filter_model.convert_path_to_child_path(to_path)      
             to_iter = to_model.get_iter(to_path)
         else:
             to_path = None
@@ -51,7 +60,10 @@ class BaseTree(gtk.TreeView):
             
         
         from_tree = drag_context.get_source_widget()
-        from_model, from_iter = from_tree.get_selection().get_selected()
+        from_filter_model, from_paths = from_tree.get_selection().get_selected_rows()
+        from_model = from_filter_model.get_model()
+        from_path = from_filter_model.convert_path_to_child_path(from_paths[0]) 
+        from_iter = from_model.get_iter(from_path)
         
         self.iter_copy(from_model, from_iter, to_model, to_iter, to_pos)
         
