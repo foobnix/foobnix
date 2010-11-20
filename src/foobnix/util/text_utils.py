3a64d346f4194dda92d80106d5efd9964e165278
Ivan Ivanenko <ivan.ivanenko@gmail.com>
2010-11-20 21:32:49 +0200

    demo tests

--- src/foobnix/util/text_utils.py
+++ src/foobnix/util/text_utils.py
@@ -1,17 +1,46 @@
 
 def capitilize_query(line):
-    if line.startswith("http"):
+    if not line:
+        return line
+    
+    if line.startswith("http://"):
         return line
     
     line = u"" + line.strip()
     result = ""
     for word in line.split():
         result += " " + word[0].upper() + word[1:]
-    return result
+    return result.strip()
 
 def capitilize_string(src):
+    if not src:
+        return src
+        
     line = u"" + src.strip()
     result = ""
     for word in line.split():
         result += " " + word[0].upper() + word[1:].lower()
+    return result.strip()
+
+
+def smart_splitter(input, max_len):
+    if not input:
+        return input
+    
+    if max_len > len(input):
+        return input
+        
+    separators = (" " , "-" , "," , "/" , "_", "\n")    
+    result = []    
+    buffer = ""
+    for i in xrange(len(input)):
+        char = input[i]
+        buffer += char
+                
+        if len(buffer) >= max_len:
+            if char in separators:   
+                result.append(buffer.strip())
+                buffer = ""                
+    result.append(buffer[:max_len].strip())
     return result
+        
---
+++ test/test_text_utils.py
@@ -0,0 +1,46 @@
+import unittest
+from foobnix.util.text_utils import smart_splitter, capitilize_string, \
+    capitilize_query
+
+class TestCapitalizeFunctions(unittest.TestCase):
+    def test_capitilize_None(self):
+        self.assertEquals(None, capitilize_string(None))
+        self.assertEquals("", capitilize_string(""))
+        
+    def test_capitilize(self):
+        self.assertEquals(u"Madonna Music", capitilize_string("MaDoNna MUSIC"))
+
+class TestCapitalizeQueryFunctions(unittest.TestCase):
+    def test_capitilize_None(self):
+        self.assertEquals(None, capitilize_query(None))
+        self.assertEquals("", capitilize_query(""))
+        
+    def test_capitilize_url(self):
+        self.assertEquals(u"http://Madonna", capitilize_query("http://Madonna"))
+    
+    def test_capitilize_sring(self):
+        self.assertEquals(u"Ddt", capitilize_query("ddt"))
+        self.assertEquals(u"DDT", capitilize_query("DDT"))
+
+class TestSplitterFunctions(unittest.TestCase):
+    def setUp(self):
+        self.input = "abcde 1234 w2e3"
+    
+    def test_empty_string(self):
+        result = smart_splitter(None, 3)
+        self.assertEquals(None, result)
+        
+    def test_empty_len(self):
+        result = smart_splitter("100", None)
+        self.assertEquals(["100"], result)
+        
+    def test_good_splitter(self):
+        result = smart_splitter(self.input, 4)
+        self.assertEquals(["abcde", "1234", "w2e3"], result)
+    
+    def test_good_splitter1(self):
+        result = smart_splitter(self.input, 2)
+        self.assertEquals(["abcde", "1234", "w2"], result)
+
+if __name__ == '__main__':
+    unittest.main()
