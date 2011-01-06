57803265b37ed541514e2696bfde28879905b786
fain182 <fain182@gmail.com>
2011-01-06 19:04:30 +0100

    removed unuseful import

--- src/test/test_google_service.py
+++ src/test/test_google_service.py
@@ -5,11 +5,9 @@ Created on 22 нояб. 2010
 @author: ivan
 '''
 import unittest
-from foobnix.regui.service.lastfm_service import LastFmService
 from foobnix.regui.service.google_service import google_search_results
 from foobnix.thirdparty.google.translate import translate
 class TestGoogleService(unittest.TestCase):
-    lfm = LastFmService()
         
     def test_find_word(self):
         list = google_search_results("Madonna", 10)
