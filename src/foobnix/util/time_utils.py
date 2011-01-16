ee55ff0de8880c0d4334c318377c3549f8f86c1b
Ivan Ivanenko <ivan.ivanenko@gmail.com>
2011-01-16 19:31:13 +0200

    change json to simplejson

--- src/foobnix/util/time_utils.py
+++ src/foobnix/util/time_utils.py
@@ -15,8 +15,6 @@ def size2text(size):
     return size
 
 def convert_seconds_to_text(time_sec):
-        if not time_sec:
-            return ""
         time_sec = int(time_sec)
 
         hours = time_sec / (60 * 60)
