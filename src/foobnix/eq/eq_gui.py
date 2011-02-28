c9745209cc9e8ad999d82d4fff1f53d108c30651
Ivan Ivanenko <ivan.ivanenko@gmail.com>
2011-02-28 22:43:36 +0200

    fix VK

--- src/foobnix/eq/eq_gui.py
+++ src/foobnix/eq/eq_gui.py
@@ -14,6 +14,7 @@ from foobnix.helpers.menu import Popup
 from foobnix.helpers.my_widgets import ImageButton
 import copy
 from foobnix.helpers.window import ChildTopWindow
+import logging
 
 
 def label(): 
@@ -93,6 +94,9 @@ class EqWindow(ChildTopWindow, FControl):
     
     def on_save(self, *args):
         text = self.combo.get_active_text()
+        
+        logging.debug("text %s "%text)
+        
         find = False
         text_id = None 
         for model in self.models:
@@ -100,14 +104,18 @@ class EqWindow(ChildTopWindow, FControl):
                 model.set_values(self.get_active_values()[1:])
                 find = True
                 text_id = model.id
+                logging.debug("find %s "%model.id)
                 break
         
         if not find:
             self.models.append(EqModel(text, text, 0, self.get_active_values()[1:]))
             self.combo.append_text(text)
             text_id = text
+            logging.debug("not find %s "%text)
         
-        FC().eq_presets_default = text_id   
+        FC().eq_presets_default = text_id
+        logging.debug("SAVE %s "%text_id)   
+        FC().save()
         
     def get_active_values(self):
         result = []
