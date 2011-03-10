07cc028fe4342bb7649850f84735533dff14b996
Ivan Ivanenko <ivan.ivanenko@gmail.com>
2011-03-10 22:53:45 +0200

    TEST WIN LOCALE

--- src/foobnix/util/localization.py
+++ src/foobnix/util/localization.py
@@ -5,11 +5,18 @@ Created on 24  2010
 @author: ivan
 '''
 import gettext
+import locale
+import os
 
 def foobnix_localization():
     APP_NAME = "foobnix"
-    gettext.install(APP_NAME, unicode=True)
-    gettext.bindtextdomain(APP_NAME, "share/locale")
-    gettext.textdomain(APP_NAME)
+    #gettext.install(APP_NAME, unicode=True)
+    #gettext.bindtextdomain(APP_NAME, "share/locale")
+    #gettext.textdomain(APP_NAME)
+     
+    l = locale.setlocale(locale.LC_ALL, '')
+    os.environ.setdefault('LANG', locale.normalize(l))
+    t = gettext.translation(APP_NAME)
+    t.install(unicode=True) 
      
 
