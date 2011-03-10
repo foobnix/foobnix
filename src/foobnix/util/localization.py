f4f2a059c447d247eb6a60fad2e86c7b6cfc9b97
Ivan Ivanenko <ivan.ivanenko@gmail.com>
2011-03-10 22:56:14 +0200

    test translate

--- src/foobnix/util/localization.py
+++ src/foobnix/util/localization.py
@@ -11,11 +11,11 @@ import os
 def foobnix_localization():
     APP_NAME = "foobnix"
     #gettext.install(APP_NAME, unicode=True)
-    #gettext.bindtextdomain(APP_NAME, "share/locale")
     #gettext.textdomain(APP_NAME)
-     
+    
     l = locale.setlocale(locale.LC_ALL, '')
     os.environ.setdefault('LANG', locale.normalize(l))
+    gettext.bindtextdomain(APP_NAME, "share/locale")
     t = gettext.translation(APP_NAME)
     t.install(unicode=True) 
      
