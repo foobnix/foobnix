12b091533c066cbd692f9f16d00c9d37d215188e
Ivan Ivanenko <ivan.ivanenko@gmail.com>
2011-06-03 14:07:22 +0300

    net wrapper refactoring

--- src/foobnix/util/net_wrapper.py
+++ src/foobnix/util/net_wrapper.py
@@ -14,38 +14,26 @@ from subprocess import Popen, PIPE
 
 class NetWrapper():
     def __init__(self):
-        self.stop = False
+        self.flag = True
         self.start_ping()
         self.is_connected = False
         
-    
     def start_ping(self):
-        self.stop = False
-        thread.start_new_thread(self.ping, ()) 
-        
-        """ OR USE SO METHOD (but thread must be only daemon thread):
-        import threading
-        ping_thread = threading.Thread(target=self.ping)
-        ping_thread.daemon = True
-        ping_thread.start()"""
-        
-        
+        self.flag = True
+        thread.start_new_thread(self.ping, ())
+         
     def stop_ping(self):
-        self.stop = True
+        self.flag = False
             
     def ping(self):
         def task(sp):
             time.sleep(10)
             if sp.returncode == None: #mistake 'if not sp.returncode:'
                 sp.kill()
-                self.set_connection(False)
+                self.is_connected = False
                            
-        list = ['ping', 'google.com', '-c 1']
-        
-        while True:
-            if self.stop:
-                return
-            sp = Popen(list, stdout=PIPE, stderr=PIPE)
+        sp = Popen(['ping', 'google.com', '-c 1'], stdout=PIPE, stderr=PIPE)
+        while self.flag:
             timer = Thread(target=task, args=(sp,))    
             timer.start()
             out, err = sp.communicate()
@@ -53,6 +41,7 @@ class NetWrapper():
                 self.is_connected = False
             elif out:
                 self.is_connected = True
+            
             time.sleep(20)
       
     def break_connection(self):
@@ -61,12 +50,11 @@ class NetWrapper():
           
     def restore_connection(self):
         self.start_ping()
-    
-    
+        
     def execute(self,func, *args):
         if not self.is_connected:
-            logging.info("Connection to internet NOT exists")
+            logging.warning("No internet connection")
             return None
         else:
-            logging.info("connectioned to internet success")
+            logging.info("Success internet connection")
             return func(*args) if args else func()
