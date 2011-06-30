08fe0f10d9f963b7b10f5f38f3974b4a8ea54c53
zavlab1 <zavlab1@gmail.com>
2011-06-30 10:14:14 +0400

    Clear prints

--- src/foobnix/util/net_wrapper.py
+++ src/foobnix/util/net_wrapper.py
@@ -32,20 +32,16 @@ class NetWrapper():
             thread.start_new_thread(self.ping, ())
             
     def ping(self):
-        print "start ping"
         def task(sp):
             i = 0
             while i < 15:
                 if sp.poll() != None:
-                    print "in sp.poll()"
                     return
                 else:
-                    print "in else", i
                     i += 1
                     time.sleep(0.5)
-            #if sp.returncode == None: #mistake 'if not sp.returncode:'
+            
             if not self.out:
-                print "in if not self.out"
                 self.is_connected = False
                 logging.debug("internet is not connected \"not self.out\" ")            
                 sp.kill()
@@ -62,22 +58,16 @@ class NetWrapper():
             timer = Thread(target=task, args=(sp,))    
             timer.start()
             self.out, self.error = sp.communicate()
-            print "after communicate"
             if self.error:
-                print "self.error", self.error
                 self.is_connected = False
                 logging.debug("internet is not connected - error")
             elif self.out:
-                print "len", len(self.out.split('\n'))
                 if "100%" in self.out:
-                    print "in len"
                     self.is_connected = False
                     logging.debug("internet is not connected - error")
-                print "self.out", self.out
                 self.is_connected = True
             time.sleep(2)
-            print self.is_internet()
-    
+                
     def disconnect_dialog(self):
         dialog_entry.info_dialog(_("Internet Connection"), _("Foobnix not connected \n or internet not available. \n Please try again a little bit later."))
     
@@ -90,12 +80,10 @@ class NetWrapper():
     def execute(self,func, *args):
         if self.is_connected:
             self.previous_connect = True
-            print "Success internet connection"
             logging.info("Success internet connection")
             return func(*args) if args else func()
         else:
             if self.previous_connect:
-                print "dialog"
-            self.disconnect_dialog    
+                self.disconnect_dialog()    
             logging.warning("No internet connection")
             return None
