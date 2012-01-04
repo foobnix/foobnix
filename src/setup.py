15ce8a71f1d1fbcb1849a3c2876f93bda6a0c901
Ivan Ivanenko <ivan.ivanenko@gmail.com>
2012-01-04 21:57:59 +0200

    merge

--- src/setup.py
+++ src/setup.py
@@ -7,7 +7,7 @@ from test.all import run_all_tests
 if os.name == 'nt':
     import py2exe #@UnresolvedImport @UnusedImport
 
-VERSION = "2.5.34"
+VERSION = "2.5.35"
 
 data_files = [
     ('share/applications', ['foobnix.desktop']),
