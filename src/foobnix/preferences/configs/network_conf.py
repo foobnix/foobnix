3d15cdbf45e1646b68fb875908bcbaf42f6145b2
zavlab1 <zavlab1@gmail.com>
2012-10-16 19:39:20 +0400

    Revert "Universal opener for https and http"
    This reverts commit 019f2f6e99d7a4592bbbd5748bb7ea5dffd0c7a6.

--- src/foobnix/preferences/configs/network_conf.py
+++ src/foobnix/preferences/configs/network_conf.py
@@ -153,8 +153,8 @@ class NetworkConfig(ConfigPlugin):
         self.enable_proxy.set_active(FC().proxy_enable)
         self.frame.set_sensitive(FC().proxy_enable)
         
-        if  FC().proxy_enable and FC().proxy_url:
-            set_proxy_settings()
+        if  self.enable_proxy.get_active():
+            FC().cookie = None
         
         if FC().proxy_url:
             self.proxy_server.set_text(FC().proxy_url)
