bf62f34f7257b0dd2108602b0b9996e3cb6e5582
Viktor Suprun <popsul1993@gmail.com>
2013-02-12 22:58:08 +1100

    Fixed charset corrector bug, when some unicode symbols be converted into illegal cyrillic symbols. Some PEP-8 fixes.

--- src/foobnix/util/id3_util.py
+++ src/foobnix/util/id3_util.py
@@ -5,6 +5,7 @@ Created on 24 нояб. 2010
 @author: ivan
 '''
 from _bsddb import api
+import chardet
 import os
 import re
 import logging
@@ -29,17 +30,20 @@ RUS_ALPHABITE = "абвгдеёжзийклмнопрстуфхцчшщъыьэ�
 
 
 def decode_cp866(text):
+    """
+    Deprecated
+    """
     try:
         for i in text:
             if i.lower() in RUS_ALPHABITE:
                 return text
-        
+
         if not is_cp866(text):
             return text
-             
+
         decode_text = text.decode('cp866')
-        
-        if u"├" in decode_text:   
+
+        if u"├" in decode_text:
             decode_text = decode_text.replace(
                         u"\u252c", u'ё').replace(
                         u"├", "").replace(
@@ -65,16 +69,29 @@ def decode_cp866(text):
         pass
     return text
 
+
 def is_cp866(text):
-    '''del figures, punctuation marks and unrecognized text (so as cp866)'''
+    """
+    Deprecated
+    """
+    """del figures, punctuation marks and unrecognized text (so as cp866)"""
     only_alphabite = re.sub('[\d\W]', '', text)
-    
+
     '''del only figures and punctuation marks'''
     without_punctuation = re.sub('[\d.,/_\-\^#$%&*()+=<>;:\'\"|]', '', text)
-    
+
     """if unrecognized characters are more half of all 
     alphabetic characters, very likely code text is cp866"""
     return len(only_alphabite) <= len(without_punctuation)/2
+
+
+def correct_encoding(text):
+    try:
+        text = text.encode('cp1252').decode('cp1251')
+    except:
+        pass
+    return text
+
 
 def udpate_id3_for_beans(beans):
     for bean in beans:
@@ -95,10 +112,11 @@ def udpate_id3_for_beans(beans):
                 bean.text = os.path.basename(bean.text)
     return beans
 
+
 def udpate_id3(bean):
     if bean and bean.path and os.path.isfile(bean.path):
         try:
-            audio = get_mutagen_audio(bean.path)            
+            audio = get_mutagen_audio(bean.path)
         except Exception, e:
             logging.warn("ID3 NOT MP3" + str(e) + bean.path)
             return bean
@@ -108,27 +126,26 @@ def udpate_id3(bean):
                 if audio.has_key('\xa9nam'): bean.title = audio["\xa9nam"][0]
                 if audio.has_key('\xa9alb'): bean.album = audio["\xa9alb"][0]
                 if audio.has_key('\xa9wrt'): bean.composer = audio["\xa9wrt"][0]
-                if audio.has_key('trkn'): 
+                if audio.has_key('trkn'):
                     if not FC().numbering_by_order:
                         bean.tracknumber = audio['trkn'][0]
             else:
-                if audio.has_key('artist'): bean.artist = decode_cp866(audio["artist"][0])
-                if audio.has_key('title'): bean.title = decode_cp866(audio["title"][0])
-                if audio.has_key('album'): bean.album = decode_cp866(audio["album"][0])
-                if audio.has_key('composer'): bean.composer = decode_cp866(audio['composer'][0])
+                if audio.has_key('artist'): bean.artist = correct_encoding(audio["artist"][0])
+                if audio.has_key('title'): bean.title = correct_encoding(audio["title"][0])
+                if audio.has_key('album'): bean.album = correct_encoding(audio["album"][0])
+                if audio.has_key('composer'): bean.composer = correct_encoding(audio['composer'][0])
                 if audio.has_key('tracknumber'):
                     if not FC().numbering_by_order:
                         bean.tracknumber = audio["tracknumber"][0]
-                        bean.tracknumber
-        
+
         duration_sec = bean.duration_sec
-        
+
         if not bean.duration_sec and audio.info.length:
             duration_sec = int(audio.info.length)
-        
+
         if audio.info.__dict__:
             bean.info = normalized_info(audio.info, bean)
-                       
+
         if bean.artist or bean.title:
             if bean.artist and bean.title:
                 pass
@@ -137,18 +154,18 @@ def udpate_id3(bean):
             elif bean.title:
                 bean.artist = _("Unknown artist")
             bean.text = bean.artist + " - " + bean.title
-                    
+
         if bean.tracknumber:
             try:
                 bean.tracknumber = int(bean.tracknumber)
             except:
                 bean.tracknumber = ""
-        
+
         bean = update_bean_from_normalized_text(bean)
-            
         bean.time = convert_seconds_to_text(duration_sec)
-        
+
     return bean
+
 
 def normalized_info(info, bean):
     list = info.pprint().split(", ")
@@ -172,6 +189,7 @@ def normalized_info(info, bean):
     new_list.append(size)
     return " | ".join(new_list)
 
+
 def get_support_music_beans_from_all(beans):
     result = []
     for bean in beans:
@@ -183,7 +201,7 @@ def get_support_music_beans_from_all(beans):
             result.append(bean)
         else:
             result.append(bean)
-    
+
     return result
 
 
@@ -195,6 +213,7 @@ def add_update_image_paths(beans):
                 bean.image = get_image_by_path(bean.path)
     return beans
 
+
 def _get_extension_by_mime(mime):
     if mime == "image/jpeg" or mime == "image/jpg":
         return ".jpg"
@@ -203,20 +222,24 @@ def _get_extension_by_mime(mime):
     logging.warning("Unknown cover mime-type: %s" % mime)
     return None
 
+
 def _get_pic_from_mp3(audio):
     apics = [k for k in audio.keys() if k.startswith("APIC:")]
     if apics:
         return audio[apics[0]]
     return None
 
+
 def _get_pic_from_flac(audio):
     if audio.pictures:
         return audio.pictures[0]
     return None
 
+
 def _get_pic_from_ape(audio):
     """Not implemented yet"""
     return None
+
 
 def set_cover_from_tags(bean):
     try:
