703b9a61f107f697f24d56a9dae1f73d521298b4
zavlab1 <zavlab1@gmail.com>
2013-04-19 01:04:34 +0400

    new drag-and-drop begin development

--- src/foobnix/regui/treeview/dragdrop_tree.py
+++ src/foobnix/regui/treeview/dragdrop_tree.py
@@ -14,6 +14,7 @@ import thread
 import logging
 import os.path
 import threading
+import collections
 
 from foobnix.fc.fc import FC
 from foobnix.fc.fc_cache import FCache
@@ -27,6 +28,7 @@ from foobnix.util.m3u_utils import m3u_reader, is_m3u
 from foobnix.util.id3_file import update_id3_wind_filtering
 from foobnix.util.iso_util import get_beans_from_iso_wv
 from foobnix.regui.model import FModel, FTreeModel
+import json
 
 
 VIEW_PLAIN = 0
@@ -39,6 +41,8 @@ class DragDropTree(Gtk.TreeView):
         Gtk.TreeView.__init__(self)
         
         self.connect("drag-begin", self.on_drag_begin)
+        self.connect("drag-data-get", self.on_drag_data_get)
+        self.connect("drag-data-received", self.on_drag_data_received)
         self.connect("drag-drop", self.on_drag_drop)
         self.connect("drag-motion", self.on_drag_motion)
         self.copy = False
@@ -46,7 +50,132 @@ class DragDropTree(Gtk.TreeView):
         """init values"""
         self.hash = {None: None}
         self.current_view = None
-    
+    '''    
+    def on_drag_data_received(self, widget, drag_context, x, y, selection, info, time):
+        print "on_drag_data_received"
+        print info
+        print selection.get_data()
+        #print selection.get_text()
+        self.stop_emission('drag-data-received')'''
+        
+
+    def on_drag_data_received(self, treeview, context, x, y, selection, info,\
+                          timestamp):
+        
+        ordered_dict = json.loads(selection.get_data())
+        print "received", ordered_dict
+        model = treeview.get_model()
+        drop_info = treeview.get_dest_row_at_pos(x, y)
+        if drop_info:
+            path, position = drop_info
+            iter = model.get_iter(path)
+            if (position == Gtk.TREE_VIEW_DROP_BEFORE
+                or position == Gtk.TREE_VIEW_DROP_INTO_OR_BEFORE):
+                model.insert_before(iter, [data])
+            else:
+                model.insert_after(iter, [data])
+        else:
+            model.append([data])
+        if context.action == Gtk.DragAction.MOVE:
+            context.finish(True, True, time)
+        return
+        
+        """ Handle a drop situation.
+
+        First of all, we need to get id of node which should accept
+        all draged nodes as their new children. If there is no node,
+        drop to root node.
+
+        Deserialize iterators of dragged nodes (see self.on_drag_data_get())
+        Info parameter determines which target was used:
+            * info == 0 => internal DND within this TreeView
+            * info > 0 => external DND
+
+        In case of internal DND we just use Tree.move_node().
+        In case of external DND we call function associated with that DND
+        set by self.set_dnd_external()
+        """
+        print "on_drag_data_received", treeview, context, x, y, selection, info, timestamp
+
+        model = treeview.get_model()
+        destination_iter = None
+        destination_tid = None
+        drop_info = treeview.get_dest_row_at_pos(x, y)
+        if drop_info:
+            path, position = drop_info
+            destination_iter = model.get_iter(path)
+            if destination_iter:
+                destination_tid = model.get_value(destination_iter, 0)
+
+        # Get dragged iter as a TaskTreeModel iter
+        # If there is no selected task (empty selection.data), 
+        # explictly skip handling it (set to empty list)
+        data = selection.get_data()
+        if data == '':
+            iters = []
+        else:
+            iters = data.split(',')
+
+        dragged_iters = []
+        for iter in iters:
+            print "Info", info
+            if info == 0:
+                try:
+                    print 99, model.get_iter_from_string(iter)
+                    dragged_iters.append(model.get_iter_from_string(iter))
+                except ValueError:
+                    #I hate to silently fail but we have no choice.
+                    #It means that the iter is not good.
+                    #Thanks shitty Gtk API for not allowing us to test the string
+                    print "Shitty iter", iter
+                    dragged_iter = None
+
+            #elif info in dnd_external_targets and destination_tid:
+                #f = dnd_external_targets[info][1]
+
+                #src_model = context.get_source_widget().get_model()
+                #dragged_iters.append(src_model.get_iter_from_string(iter))
+
+
+        for dragged_iter in dragged_iters:
+            if info == 0:
+                if dragged_iter and model.iter_is_valid(dragged_iter):
+                    dragged_tid = model.get_value(dragged_iter, 0)
+                    try:
+                        row = []
+                        for i in range(model.get_n_columns()):
+                            row.append(model.get_value(dragged_iter, i))
+                        #tree.move_node(dragged_tid, new_parent_id=destination_tid)
+                        print "move_after(%s, %s) ~ (%s, %s)" % (dragged_iter, destination_iter, dragged_tid, destination_tid)
+                        #model.move_after(dragged_iter, destination_iter)
+                        model.insert(destination_iter, -1, row)
+                        model.remove(dragged_iter)
+                    except Exception, e:
+                        print 'Problem with dragging: %s' % e
+            #elif info in dnd_external_targets and destination_tid:    
+                #source = src_model.get_value(dragged_iter,0)
+                # Handle external Drag'n'Drop
+                #f(source, destination_tid)
+        
+    def on_drag_data_get(self, source_tree, drag_context, selection, info, time):
+        print "on_drag_data_get", info
+        treeselection = source_tree.get_selection()
+        ff_model, ff_paths = treeselection.get_selected_rows()
+        #iters = [ff_model.get_iter(ff_path) for ff_path in ff_paths]
+        #iter_str = ','.join([ff_model.get_string_from_iter(iter) for iter in iters])
+        #selection.set(selection.get_target(), 0, iter_str)
+        #print "Sending", iter_str
+        dict = self.get_dict_from_selected(ff_model, ff_paths)
+        print dict
+        string_variable = json.dumps(dict)
+        print string_variable
+        selection.set(selection.get_target(), 0, string_variable)
+            #data = bytes(ff_model.get_value(ff_iter, 3))
+            #selection.set(selection.get_target(), 8, data)
+            #selection.set_text(data, -1)
+            #return
+            #Gtk.tree_set_row_drag_data(selection, ff_model, ff_path)'''
+
     def on_drag_begin(self, source_widget, drag_context):
         ff_model, ff_paths = source_widget.get_selection().get_selected_rows()  # @UnusedVariable
         global LAST_DND_SOURCE_TREE
@@ -61,11 +190,14 @@ class DragDropTree(Gtk.TreeView):
         return True
                 
     def configure_recive_drag(self):
-        self.enable_model_drag_dest([("text/uri-list", 0, 0)], Gdk.DragAction.COPY | Gdk.DragAction.MOVE) # @UndefinedVariable
+        self.enable_model_drag_dest([('MY_TREE_MODEL_ROW', Gtk.TargetFlags.SAME_WIDGET, 0)], Gdk.DragAction.COPY | Gdk.DragAction.MOVE) # @UndefinedVariable
+        self.drag_dest_add_text_targets()
+        #self.drag_dest_set(Gtk.DestDefaults.MOTION | Gtk.DestDefaults.DROP, [Gtk.TargetEntry.new("text/uri-list", 1, 80)], Gdk.DragAction.COPY | Gdk.DragAction.MOVE)
     
     def configure_send_drag(self):
-        #self.enable_model_drag_source(Gdk.ModifierType.BUTTON1_MASK, [("text/uri-list", 0, 0)], Gdk.ModifierType.ACTION_COPY | Gdk.ModifierType.ACTION_MOVE) #@UndefinedVariable
-        self.drag_source_set(Gdk.ModifierType.BUTTON1_MASK, [Gtk.TargetEntry.new("text/uri-list", 0, 0)], Gdk.DragAction.COPY | Gdk.DragAction.MOVE) #@UndefinedVariable
+        #self.enable_model_drag_source(Gdk.ModifierType.BUTTON1_MASK, [("text/uri-list", 0, 0)], Gdk.DragAction.COPY | Gdk.DragAction.MOVE) #@UndefinedVariable
+        self.drag_source_set(Gdk.ModifierType.BUTTON1_MASK, [Gtk.TargetEntry.new('MY_TREE_MODEL_ROW', Gtk.TargetFlags.SAME_WIDGET, 0)], Gdk.DragAction.COPY | Gdk.DragAction.MOVE) #@UndefinedVariable
+        self.drag_source_add_text_targets()
     
     def append_all(self, beans):
         logging.debug("begin append all")
@@ -139,8 +271,12 @@ class DragDropTree(Gtk.TreeView):
                 result.append(child_row)        
         return result
     
-    def on_drag_drop(self, to_tree, drag_context, x, y, selection):
+    def on_drag_drop(self, to_tree, context, x, y, time):
         # ff - from_filter
+        to_tree.drag_get_data(context, context.list_targets()[-1], time)
+        return True
+        
+        self.stop_emission('drag-drop')
         self.copy = drag_context.get_selected_action()
         to_filter_model = to_tree.get_model()
         to_model = to_filter_model.get_model()
@@ -540,7 +676,7 @@ class DragDropTree(Gtk.TreeView):
                 dest_folder = os.path.dirname(previous_path) 
             else:
                 logging.debug("item is top in tree")
-                next_path = to_f_model.get_value(to_f_model.get_iter_root(), self.path[0])
+                next_path = to_f_model.get_value(to_f_model.get_iter_first(), self.path[0])
                 dest_folder = os.path.dirname(next_path)
         else:
             dest_folder = to_f_model.get_value(parent_iter, self.path[0])
@@ -813,3 +949,31 @@ class DragDropTree(Gtk.TreeView):
         
         parent_iter = self.model.append(parent_iter_exists, row)
         self.hash[row[self.level[0]]] = parent_iter
+    
+    def get_dict_from_selected(self, model, paths):
+        dict = collections.OrderedDict()
+        for i, path in enumerate(paths):
+            print i
+            iter = model.get_iter(path)
+            print "iter", iter
+            for i in self.get_list_of_iters_with_children(model, iter):
+                print "in for", model.get_string_from_iter(i)
+                dict[model.get_string_from_iter(i)] = self.get_row_from_iter(model, i)
+        return dict
+    
+    def get_row_from_iter(self, model, iter):
+        row = []
+        for num in xrange(model.get_n_columns()):
+            row.append(model.get_value(iter, num))
+        return row
+            
+    def get_list_of_iters_with_children(self, model, iter):
+        all_iters = []
+        def task(iter):
+            all_iters.append(iter)
+            for n in xrange(model.iter_n_children(iter)):
+                child_iter = model.iter_nth_child(iter, n)
+                if child_iter:
+                    task(child_iter)
+        task(iter)
+        return all_iters
