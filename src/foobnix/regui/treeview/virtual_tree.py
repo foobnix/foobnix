0d0a29606405b19e50e302772c896cba35ee3a59
zavlab1 <zavlab1@gmail.com>
2013-04-23 16:40:54 +0400

    Drag and drop within virtual tree (storage)

--- src/foobnix/regui/treeview/virtual_tree.py
+++ src/foobnix/regui/treeview/virtual_tree.py
@@ -33,13 +33,8 @@ class VirtualTreeControl(CommonTreeControl, LoadSave):
         self.configure_recive_drag()
         
         self.set_type_tree()
-    
-    
-   
-    
-    
+
     def activate_perspective(self):
-   
         FC().left_perspective = LEFT_PERSPECTIVE_VIRTUAL
     
     def on_key_release(self, w, e):
@@ -49,7 +44,7 @@ class VirtualTreeControl(CommonTreeControl, LoadSave):
     def on_drag_drop_finish(self):
         FCache().cache_virtual_tree_beans = self.get_all_beans()
         FC().save()        
-        
+
     def on_button_press(self, w, e):
         if is_empty_click(w, e):
             w.get_selection().unselect_all()
@@ -75,7 +70,7 @@ class VirtualTreeControl(CommonTreeControl, LoadSave):
                 #menu.add_item(_("Save as"), Gtk.STOCK_SAVE_AS, None, None)
                 #menu.add_item(_("Open as"), Gtk.STOCK_OPEN, None, None)
                 menu.show(e)
-    
+
     def create_playlist(self):
         name = one_line_dialog(_("Create new playlist"), self.controls.main_window, message_text1=_("Enter playlist name"))
         if not name:
@@ -88,7 +83,7 @@ class VirtualTreeControl(CommonTreeControl, LoadSave):
             else:
                 folder_bean.add_parent(bean.level)
         self.append(folder_bean)      
-    
+
     def rename_selected(self):
         bean = self.get_selected_bean()
         name = one_line_dialog(_("Rename Dialog"), self.controls.main_window,
@@ -98,7 +93,7 @@ class VirtualTreeControl(CommonTreeControl, LoadSave):
         rows = self.find_rows_by_element(self.UUID, bean.UUID)
         if rows:
             rows[0][self.text[0]] = name
-    
+
     def on_load(self):
         self.scroll.hide()
         self.populate_all(FCache().cache_virtual_tree_beans)
@@ -113,60 +108,68 @@ class VirtualTreeControl(CommonTreeControl, LoadSave):
             
         self.expand_updated(set_expand_path)
         self.selection_changed(set_selected_path)
-    
+
     def on_quit(self):
         FCache().cache_virtual_tree_beans = self.get_all_beans()
-    
-    
 
-    
     def on_drag_data_received(self, treeview, context, x, y, selection, info, timestamp):
         logging.debug('Storage on_drag_data_received')
         model = self.get_model().get_model()
         drop_info = self.get_dest_row_at_pos(x, y)
-        
+
         # ff - from_filter
         ff_tree = Gtk.drag_get_source_widget(context)
         ff_model, ff_paths = ff_tree.get_selection().get_selected_rows()
         treerows = [ff_model[ff_path] for ff_path in ff_paths]
-        
         if drop_info:
             path, position = drop_info
             iter = model.get_iter(path)
             if (position == Gtk.TREE_VIEW_DROP_INTO_OR_BEFORE or
                 position == Gtk.TREE_VIEW_DROP_INTO_OR_AFTER):
                 self.model[path][self.font[0]] = 'bold'
-          
-        for  treerow in treerows:
-            row = [col for col in treerow]
-            if drop_info:
-                if (position == Gtk.TREE_VIEW_DROP_BEFORE):
-                    new_iter = model.insert_before(None, iter, row)
-                elif (position == Gtk.TREE_VIEW_DROP_INTO_OR_BEFORE or
-                    position == Gtk.TREE_VIEW_DROP_INTO_OR_AFTER):
-                    new_iter = model.append(iter, row)
+
+        if self == ff_tree:
+            ff_row_refs = [Gtk.TreeRowReference.new(ff_model, ff_path) for ff_path in ff_paths]
+            def task(treerow, new_iter):
+                    for ch_row in treerow.iterchildren():
+                        niter = model.append(new_iter, [col for col in ch_row])
+                        task(ch_row, niter)
+            for treerow, ref in zip(treerows, ff_row_refs):
+                row = [col for col in treerow]
+                if drop_info:
+                    if (position == Gtk.TREE_VIEW_DROP_BEFORE):
+                        new_iter = model.insert_before(None, iter, row)
+                    elif (position == Gtk.TREE_VIEW_DROP_INTO_OR_BEFORE or
+                          position == Gtk.TREE_VIEW_DROP_INTO_OR_AFTER):
+                        new_iter = model.append(iter, row)
+                    else:
+                        new_iter = model.insert_after(None, iter, row)
+                        iter = model.iter_next(iter)
                 else:
-                    new_iter = model.insert_after(None, iter, row)
-                    iter = model.iter_next(iter)
-            else:
-                new_iter = model.append(None, row)
-            if len(treerows) == 1 and treerow[self.font[0]] == 'bold':
-                while treerow.next[self.font[0]] != 'bold':
-                    treerow = treerow.next
-                    treerows.append(treerow)
-                drop_info = True
-                iter = new_iter
-                position = Gtk.TREE_VIEW_DROP_INTO_OR_AFTER
-        #if self is ff_tree:
+                    new_iter = model.append(None, row)
+                treerow = model[ref.get_path()] # reinitialize
+                
+                task(treerow, new_iter)       
+        else:
+            for  treerow in treerows:
+                row = [col for col in treerow]
+                if drop_info:
+                    if (position == Gtk.TREE_VIEW_DROP_BEFORE):
+                        new_iter = model.insert_before(None, iter, row)
+                    elif (position == Gtk.TREE_VIEW_DROP_INTO_OR_BEFORE or
+                          position == Gtk.TREE_VIEW_DROP_INTO_OR_AFTER):
+                        new_iter = model.append(iter, row)
+                    else:
+                        new_iter = model.insert_after(None, iter, row)
+                        iter = model.iter_next(iter)
+                else:
+                    new_iter = model.append(None, row)
+                if len(treerows) == 1 and treerow[self.font[0]] == 'bold':
+                    while treerow.next[self.font[0]] != 'bold':
+                        treerow = treerow.next
+                        treerows.append(treerow)
+                        drop_info = True
+                        iter = new_iter
+                        position = Gtk.TREE_VIEW_DROP_INTO_OR_AFTER
+
         self.stop_emission('drag-data-received')
-        
-    def get_dict_from_selected(self, model, paths):
-        dict = collections.OrderedDict()
-        for i, path in enumerate(paths):
-            #print i
-            iter = model.get_iter(path)
-            #print "iter", iter
-            for i in self.get_list_of_iters_with_children(model, iter):
-                #print "in for", model.get_string_from_iter(i)
-                dict[model.get_string_from_iter(i)] = self.get_row_from_iter(model, i)
-        return dict
