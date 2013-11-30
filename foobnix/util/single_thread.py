e5882f8b2571e4b14bbc29493c5d626ef388e465
Sergey I. Mokhov <mokhovs@mte-telecom.ru>
2013-11-30 10:15:27 +0400

    fix thread.error: release unlocked lock

--- foobnix/util/single_thread.py
+++ foobnix/util/single_thread.py
@@ -43,6 +43,7 @@ class SingleThread():
                 thread.start_new_thread(self._thread_task, (method, args))  
     
     def _thread_task(self, method, args, with_lock=True):
+        self.lock.acquire()
         try:
             if method and args:
                 method(args)
@@ -57,4 +58,3 @@ class SingleThread():
                 if self.progressbar:
                     self.progressbar.stop()        
                 self.lock.release()
-            
