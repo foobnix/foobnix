f60f8da75272188a1d59734a0c72d4b6b14184dc
zavlab1 <zavlab1@gmail.com>
2014-11-22 02:08:47 +0300

    Fix lyricsmania.com parser

--- foobnix/gui/service/lyrics_parsing_service.py
+++ foobnix/gui/service/lyrics_parsing_service.py
@@ -14,6 +14,7 @@ class LyricsFinder(HTMLParser):
         HTMLParser.__init__(self)
         self.data = []
         self.needed_tag = 0
+        self.inner_tags_counter = 0
         self.tagname = tagname
         self.attr = attr
         self.attr_value = attr_value
@@ -21,8 +22,8 @@ class LyricsFinder(HTMLParser):
     def get_lyrics_from_lyricsmania(self, artist, title):
         base = "http://www.lyricsmania.com/"
         self.tagname = 'div'
-        self.attr = 'id'
-        self.attr_value = 'songlyrics_h'
+        self.attr = 'class'
+        self.attr_value = 'lyrics-body'
         title = title.encode('utf-8').strip().replace(" ", "_").replace("/", "-")
         title = urllib.quote(title)
         artist = artist.encode('utf-8').strip().replace(" ", "_").replace("/", "-")
@@ -30,7 +31,7 @@ class LyricsFinder(HTMLParser):
         result = urllib.urlopen(base + title + "_lyrics_" + artist + ".html").read()
         result = result.replace('&#039;', '!apostrophe!')
         self.feed(result)
-        return "\n".join(self.data).replace('!apostrophe!', '&#039;')
+        return "\n".join(self.data[7:]).replace('!apostrophe!', '&#039;')
 
     def get_lyrics_from_megalyrics(self, artist, title):
         base = "http://megalyrics.ru/lyric/"
@@ -47,12 +48,20 @@ class LyricsFinder(HTMLParser):
         return "\n".join(self.data).replace('!apostrophe!', "\'")
 
     def handle_starttag(self, tag, attrs):
+        if self.needed_tag and tag not in ['br', 'img']: #br and img doesn't have endtags
+            #if inside of needed tag is other tags that avoid handle its endtags
+            self.inner_tags_counter += 1
+            return
         if tag == self.tagname:
             for name, value in attrs:
                 if name == self.attr and value == self.attr_value:
                     self.needed_tag = 1
+                    return
 
     def handle_endtag(self, tag):
+        if self.inner_tags_counter:
+            self.inner_tags_counter -= 1
+            return
         if tag == self.tagname and self.needed_tag:
             self.needed_tag = 0
 
@@ -73,21 +82,18 @@ def get_lyrics_by_parsing(artist, title):
     lyrics_finder.data = []
     text = None
     try:
-        logging.debug("Try to get lyrics from lyricsmania.com")
-        text = lyrics_finder.get_lyrics_from_lyricsmania(artist, title)
+        logging.debug("Try to get lyrics from megalyrics.ru")
+        text = lyrics_finder.get_lyrics_from_megalyrics(artist, title)
     except:
-        logging.info("Error occurred when getting lyrics from lyricsmania.com")
+        logging.info("Error occurred when getting lyrics from megalyrics.ru")
 
     if not text:
         try:
-            logging.debug("Try to get lyrics from megalyrics.ru")
-            text = lyrics_finder.get_lyrics_from_megalyrics(artist, title)
-        except Exception, e:
-            import sys
-            import traceback
-            exc_type, exc_value, exc_traceback = sys.exc_info()
-            traceback.print_exception(exc_type, exc_value, exc_traceback, file=sys.stdout)
-            logging.info("Error occurred when getting lyrics from megalyrics.ru")
+            logging.debug("Try to get lyrics from lyricsmania.com")
+            text = lyrics_finder.get_lyrics_from_lyricsmania(artist, title)
+        except:
+            logging.info("Error occurred when getting lyrics from lyricsmania.com")
+
 
     lyrics_finder.reset()
     return text
