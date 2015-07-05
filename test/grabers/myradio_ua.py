8084461da9f9617a836f5319d78215da69ba2038
Ivan Ivanenko <ivan.ivanenko@gmail.com>
2015-07-05 19:45:51 +0300

    update myradio 192 kb/s

--- test/grabers/myradio_ua.py
+++ test/grabers/myradio_ua.py
@@ -7,7 +7,7 @@ Created on 16  2010
 import urllib2
 import logging
 import re
-site = "http://myradio.ua/player/50"
+site = "http://myradio.ua/player/7"
 
 
 def load_urls_name_page():
@@ -16,24 +16,32 @@ def load_urls_name_page():
     result = {}  
     file = open("MYRADIO_UA.fpl", "w")
     for line in data.split("\n"):
-        line = line.decode("cp1251").decode('utf8')
-        reg_all = "([-\w0-9,. ]*)"
-        findall = re.findall('name="'+reg_all+'" value="([0-9]*)" external="0" mount="'+reg_all+'"', line, re.IGNORECASE | re.UNICODE)
-        if findall:
-            name = findall[0][0]
-            url = findall[0][2]
-            out = name + " = " + "http://relay.myradio.ua/" + url + "128.mp3 \n"
-            print out
-            file.write(out)
+        line = line.decode('utf8')
+
+        if "<a href" in line and "mount" in line:
+            reg_all = "([-\w0-9,. ]*)"
+            findall = re.findall('name="'+reg_all+'"', line, re.IGNORECASE | re.UNICODE)
+            print findall
+            if findall:
+                name = findall[0]
+
+                findall = re.findall('mount="'+reg_all+'"', line, re.IGNORECASE | re.UNICODE)
+                if findall:
+                    url = findall[0]
+                    out = name + " = " + "http://relay.myradio.ua/" + url + "192.mp3 \n"
+                    print out
+                    file.write(out.encode("utf-8", "replace"))
         
 
 def test():
     reg_all = "([-\w/0-9,. ]*)"
-    url = """<a onclick="MRPlayer.changeFormat(this);" id="format_62" name="фіasdf-123" value="62" external="0" mount="eurovision2010" server="1" class="radio__dropdown-link" slogan="Музыка ежегодного телевизионного конкурса" url="eurovision" comments="40" fm="0">Евровидение</a>"""
-    findall = re.findall('name="'+reg_all+'" value="([0-9]*)" external="0" mount="'+reg_all+'"', url.decode('utf8'),re.UNICODE)
-    if findall:
-        print findall
+    url = """ <a href="/player/4" name="Русские хиты 90-х" onclick="MRPlayer.changeFormat(this);" id="format_4" mount="russkie-hity-90-h" server="2" external="0" value="4" image="" slogan="Самые яркие и популярные русские хиты 90-х" url="russkie-hity-90-h" comments="170">Русские хиты 90-х</a> """
+    findall = re.findall('name="'+reg_all+'"', url.decode('utf8'),re.UNICODE)
+    print findall[0]
+
+    findall = re.findall('mount="'+reg_all+'"', url.decode('utf8'),re.UNICODE)
+    print findall[0]
              
 
-#test();
+#test()
 load_urls_name_page()
