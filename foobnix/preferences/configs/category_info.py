8dc97c47f5a8ecb6487874b8039ca7c9aa92b5fb
zavlab1 <zavlab1@gmail.com>
2016-02-14 00:27:39 +0300

    small fix

--- foobnix/preferences/configs/category_info.py
+++ foobnix/preferences/configs/category_info.py
@@ -4,21 +4,24 @@ Created on 24 авг. 2010
 
 @author: ivan
 '''
+
 from gi.repository import Gtk
+
 from foobnix.preferences.config_plugin import ConfigPlugin
+
 class CategoryInfoConfig(ConfigPlugin):
     name = _("Category Info")
     def __init__(self):
         box = Gtk.Box.new(Gtk.Orientation.VERTICAL, 0)
         box.hide()
 
-        similar_arists = Gtk.CheckButton.new_with_label(_"Show Similar Artists")
+        similar_arists = Gtk.CheckButton.new_with_label(_("Show Similar Artists"))
         similar_arists.show()
 
-        similar_song = Gtk.CheckButton.new_with_label(_"Show Similar Songs")
+        similar_song = Gtk.CheckButton.new_with_label(_("Show Similar Songs"))
         similar_song.show()
 
-        similar_tags = Gtk.CheckButton.new_with_label(_"Show Similar Tags")
+        similar_tags = Gtk.CheckButton.new_with_label(_("Show Similar Tags"))
         similar_tags.show()
 
         box.pack_start(similar_arists, False, True, 0)
